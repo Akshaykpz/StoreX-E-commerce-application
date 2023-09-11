5623ec266d697175a2f620e4c8b3514e435b83ab
unknown <akshaymayyannur@gmail.com>
2023-09-11 21:38:24 +0530

    task complted

---
+++ lib/service/test.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+// Reference to the Firestore collection
+CollectionReference productsCollection =
+    FirebaseFirestore.instance.collection('products');
+
+// Add data to Firestore
+Future<void> addProduct(String productName, double productPrice) async {
+  try {
+    await productsCollection.add({
+      'p_name': productName,
+      'p_price': productPrice,
+    });
+  } catch (e) {
+    print('Error adding product: $e');
+  }
+}
