3e1b0d46d2fa69bfcfe78711d4354756df6a40f0
unknown <akshaymayyannur@gmail.com>
2023-09-21 18:15:17 +0530

    added new

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -13,7 +14,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: (MyApp())));
 }
 
 class MyApp extends StatelessWidget {
